826ea5b4dba134f1601272a58474095bcba71cd2
Lukas Fülling <lukas@k40s.net>
2016-10-05 09:48:06 +0200

    cleanup code

--- app/src/main/java/com/github/lfuelling/lolclick/ClickActivity.java
+++ app/src/main/java/com/github/lfuelling/lolclick/ClickActivity.java
@@ -43,52 +43,52 @@ public class ClickActivity extends AppCompatActivity {
         clickBtn = (ImageButton) findViewById(R.id.click_btn);
         final Snackbar unsupportedSnackbar = Snackbar.make(clickBtn, R.string.not_supported, Snackbar.LENGTH_LONG);
         clickBtn.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            score++;
-                                            Log.d(TAG, "There's a click! New score: " + score);
-                                            scoreView.setText(String.valueOf(score));
-                                            int anInt = new Random().nextInt(score);
-                                            if (score == 1) {
-                                                Log.d(TAG, "First click! Starting stopwatch...");
-                                                stopwatch = System.currentTimeMillis();
-                                                clickBtn.startAnimation(blinkAnimation);
-                                                Toast.makeText(ClickActivity.this, R.string.keep_going, Toast.LENGTH_LONG).show();
-                                                setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
+            @Override
+            public void onClick(View v) {
+                score++;
+                Log.d(TAG, "There's a click! New score: " + score);
+                scoreView.setText(String.valueOf(score));
+                int anInt = new Random().nextInt(score);
+                if (score == 1) {
+                    Log.d(TAG, "First click! Starting stopwatch...");
+                    stopwatch = System.currentTimeMillis();
+                    clickBtn.startAnimation(blinkAnimation);
+                    Toast.makeText(ClickActivity.this, R.string.keep_going, Toast.LENGTH_LONG).show();
+                    setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
 
-                                            } else if (score % 50 == 0) {
-                                                // checking using '==' instead of '>=' because the background only needs to be set once.
-                                                setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_lol);
-                                                clickBtn.startAnimation(blinkAnimation);
-                                                String state = String.valueOf((System.currentTimeMillis() - stopwatch) / 1000);
-                                                Log.d(TAG, "We're at " + state + " now");
-                                                Toast.makeText(ClickActivity.this, "You needed " + state + " seconds!", Toast.LENGTH_LONG).show();
-                                            } else if (score % ((anInt <= 0) ? 75 : anInt) == 0) {
-                                                Log.i(TAG, "Multi 2x");
-                                                multiply(2);
-                                                setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
-                                                setBgOrWarn(multiView, unsupportedSnackbar, R.mipmap.multi2x);
-                                                multiView.setVisibility(View.VISIBLE);
-                                                multiView.startAnimation(multiAnimation);
-                                            } else if (anInt <= 0) {
-                                                // This will be interesting
-                                                Log.i(TAG, "Multi 4x");
-                                                multiply(4);
-                                                setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
-                                                setBgOrWarn(multiView, unsupportedSnackbar, R.mipmap.multi4x);
-                                                multiView.setVisibility(View.VISIBLE);
-                                                multiView.startAnimation(multiAnimation);
-                                                clickBtn.startAnimation(blinkAnimation);
-                                            } else {
-                                                clickBtn.startAnimation(pressAnimation);
-                                                if (multiView.getVisibility() == View.VISIBLE) {
-                                                    multiView.setVisibility(View.GONE);
-                                                }
-                                            }
-                                        }
+                } else if (score % 50 == 0) {
+                    // checking using '==' instead of '>=' because the background only needs to be set once.
+                    setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_lol);
+                    clickBtn.startAnimation(blinkAnimation);
+                    String state = String.valueOf((System.currentTimeMillis() - stopwatch) / 1000);
+                    Log.d(TAG, "We're at " + state + " now");
+                    Toast.makeText(ClickActivity.this, "You needed " + state + " seconds!", Toast.LENGTH_LONG).show();
+                } else if (score % ((anInt <= 0) ? 75 : anInt) == 0) {
+                    Log.i(TAG, "Multi 2x");
+                    multiply(2);
+                    setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
+                    setBgOrWarn(multiView, unsupportedSnackbar, R.mipmap.multi2x);
+                    multiView.setVisibility(View.VISIBLE);
+                    multiView.startAnimation(multiAnimation);
+                } else if (anInt <= 0) {
+                    // This will be interesting
+                    Log.i(TAG, "Multi 4x");
+                    multiply(4);
+                    setBgOrWarn(clickBtn, unsupportedSnackbar, R.mipmap.ic_gusta);
+                    setBgOrWarn(multiView, unsupportedSnackbar, R.mipmap.multi4x);
+                    multiView.setVisibility(View.VISIBLE);
+                    multiView.startAnimation(multiAnimation);
+                    clickBtn.startAnimation(blinkAnimation);
+                } else {
+                    clickBtn.startAnimation(pressAnimation);
+                    if (multiView.getVisibility() == View.VISIBLE) {
+                        multiView.setVisibility(View.GONE);
+                    }
+                }
+            }
 
 
-                                    }
+        }
 
         );
     }
